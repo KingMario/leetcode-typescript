3506deeafb06cdb975a31f3434ec224bc733e43c
Changyu Geng <gcyyq@hotmail.com>
2024-03-15 10:51:08 +0800

    Add solutions and test cases for problem 238

---
+++ src/problem-solutions/medium/238.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from "@jest/globals";
+import { TestCase, runTestSuite } from "../leetCodeTestUtility";
+import { solutions } from "./238";
+
+type Solution = (typeof solutions)[0];
+
+export const testCases: TestCase<Solution>[] = [
+  {
+    input: [[1, 2, 3, 4]],
+    expected: [24, 12, 8, 6],
+  },
+  {
+    input: [[-1, 1, 0, -3, 3]],
+    expected: [0, 0, 9, 0, 0],
+  },
+];
+
+const suiteName = [
+  "https://leetcode.com/problems/product-of-array-except-self",
+  "productExceptSelf",
+].join(" - ");
+const caseName = "should custom sort the string";
+
+expect.addEqualityTesters([
+  (a: number[], b: number[]) => a.every((v, i) => v === b[i]),
+]);
+
+runTestSuite<Solution>({ suiteName, caseName, solutions, testCases });
---
+++ src/problem-solutions/medium/238.ts
@@ -0,0 +1,35 @@
+function productExceptSelf(nums: number[]): number[] {
+  const result = new Array(nums.length).fill(1);
+
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = result[i - 1] * nums[i - 1];
+  }
+
+  let right = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= right;
+    right *= nums[i];
+  }
+
+  return result;
+}
+
+function productExceptSelfV2(nums: number[]): number[] {
+  const result = new Array(nums.length).fill(1);
+  let left = 1;
+  let right = 1;
+
+  console.log({ nums });
+
+  for (let i = 0; i < nums.length; i++) {
+    left *= nums[i - 1] ?? 1;
+    right *= nums[nums.length - i] ?? 1;
+    result[i] *= left;
+    result[nums.length - i - 1] *= right;
+    console.log({ i, left, right, result });
+  }
+
+  return result;
+}
+
+export const solutions = [productExceptSelf, productExceptSelfV2];

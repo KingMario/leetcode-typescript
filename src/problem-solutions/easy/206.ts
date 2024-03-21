02881832db3a7a044c2d014cc27f8a1a3a49c490
Changyu Geng <gcyyq@hotmail.com>
2024-03-22 07:32:14 +0800

    Add two more solutions to problem 206

--- src/problem-solutions/easy/206.ts
+++ src/problem-solutions/easy/206.ts
@@ -8,4 +8,30 @@ function reverseList(head: ListNode | null): ListNode | null {
   return arrayToListNode(listNodeToArray(head).reverse());
 }
 
-export const solutions = [reverseList];
+function reverseListV2(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  const newHead = reverseListV2(head.next);
+  head.next.next = head;
+  head.next = null;
+
+  return newHead;
+}
+
+function reverseListV3(head: ListNode | null): ListNode | null {
+  let previous: ListNode | null = null;
+  let current = head;
+
+  while (current) {
+    const next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  return previous;
+}
+
+export const solutions = [reverseList, reverseListV2, reverseListV3];

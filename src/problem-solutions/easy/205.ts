30bd3dd5c84a3d75563f116afcb44dee072d68f2
Changyu Geng <gcyyq@hotmail.com>
2024-04-02 22:15:08 +0800

    Add isIsomorphicV3 function to problem 205 solution

--- src/problem-solutions/easy/205.ts
+++ src/problem-solutions/easy/205.ts
@@ -37,4 +37,21 @@ function isIsomorphicV2(s: string, t: string): boolean {
   return true;
 }
 
-export const solutions = [isIsomorphic, isIsomorphicV2];
+function isIsomorphicV3(s: string, t: string): boolean {
+  const { length } = s;
+  const sMap = new Map<string, number>();
+  const tMap = new Map<string, number>();
+
+  for (let i = 0; i < length; i++) {
+    if (sMap.get(s[i]) !== tMap.get(t[i])) {
+      return false;
+    }
+
+    sMap.set(s[i], i);
+    tMap.set(t[i], i);
+  }
+
+  return true;
+}
+
+export const solutions = [isIsomorphic, isIsomorphicV2, isIsomorphicV3];

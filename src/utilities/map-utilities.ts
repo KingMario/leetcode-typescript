5a04002e996bd68f94b0b9999aedd5be42053aef
Changyu Geng <gcyyq@hotmail.com>
2024-04-02 14:45:21 +0800

    Refactor the map utilities to add functions that return the array/value after operation

--- src/utilities/map-utilities.ts
+++ src/utilities/map-utilities.ts
@@ -1,7 +1,17 @@
 export function addValueToArrayInMap<K, V>(map: Map<K, V[]>, key: K, value: V) {
-  const values = map.get(key) || [];
-  values.push(value);
-  map.set(key, values);
+  addValueToArrayInMapAndReturnArray(map, key, value);
+}
+
+export function addValueToArrayInMapAndReturnArray<K, V>(
+  map: Map<K, V[]>,
+  key: K,
+  value: V
+) {
+  const array = map.get(key) || [];
+  array.push(value);
+  map.set(key, array);
+
+  return array;
 }
 
 export function incrementValueInMap<K>(
@@ -9,6 +19,16 @@ export function incrementValueInMap<K>(
   key: K,
   incrementValue = 1
 ) {
-  const value = map.get(key) || 0;
-  map.set(key, value + incrementValue);
+  incrementValueInMapAndReturnValue(map, key, incrementValue);
+}
+
+export function incrementValueInMapAndReturnValue<K>(
+  map: Map<K, number>,
+  key: K,
+  incrementValue = 1
+) {
+  const value = (map.get(key) || 0) + incrementValue;
+  map.set(key, value);
+
+  return value;
 }

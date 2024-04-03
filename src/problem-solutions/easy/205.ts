954c0771f54c4231bd3704e4993fbfbecc97075e
Changyu Geng <gcyyq@hotmail.com>
2024-04-03 09:28:32 +0800

    Refactor isIsomorphic functions to improve readability and performance

--- src/problem-solutions/easy/205.ts
+++ src/problem-solutions/easy/205.ts
@@ -1,34 +1,39 @@
 import {
   incrementValueInMapAndReturnValue,
-  pushElementToArrayInMap,
+  pushElementToArrayInMapAndReturnArray,
 } from "../../utilities";
 
 function isIsomorphic(s: string, t: string): boolean {
   const { length } = s;
-  const sMap = new Map<string, number[]>();
-  const tMap = new Map<string, number[]>();
+  const sCharOccurrencesMap = new Map<string, number[]>();
+  const tCharOccurrencesMap = new Map<string, number[]>();
 
   for (let i = 0; i < length; i++) {
-    pushElementToArrayInMap(sMap, s[i], i);
-    pushElementToArrayInMap(tMap, t[i], i);
+    if (
+      pushElementToArrayInMapAndReturnArray(sCharOccurrencesMap, s[i], i).join(
+        ""
+      ) !==
+      pushElementToArrayInMapAndReturnArray(tCharOccurrencesMap, t[i], i).join(
+        ""
+      )
+    ) {
+      return false;
+    }
   }
 
-  return (
-    [...sMap.values()].map((s) => s.join(",")).join(";") ===
-    [...tMap.values()].map((t) => t.join(",")).join(";")
-  );
+  return true;
 }
 
 function isIsomorphicV2(s: string, t: string): boolean {
   const { length } = s;
-  const sMap = new Map<string, number>();
-  const tMap = new Map<string, number>();
+  const sCharFeatureMap = new Map<string, number>();
+  const tCharFeatureMap = new Map<string, number>();
 
   for (let i = 0; i < length; i++) {
     const value = (i + 1) * (i + 1);
     if (
-      incrementValueInMapAndReturnValue(sMap, s[i], value) !==
-      incrementValueInMapAndReturnValue(tMap, t[i], value)
+      incrementValueInMapAndReturnValue(sCharFeatureMap, s[i], value) !==
+      incrementValueInMapAndReturnValue(tCharFeatureMap, t[i], value)
     ) {
       return false;
     }
@@ -39,16 +44,16 @@ function isIsomorphicV2(s: string, t: string): boolean {
 
 function isIsomorphicV3(s: string, t: string): boolean {
   const { length } = s;
-  const sMap = new Map<string, number>();
-  const tMap = new Map<string, number>();
+  const sLastSeenMap = new Map<string, number>();
+  const tLastSeenMap = new Map<string, number>();
 
   for (let i = 0; i < length; i++) {
-    if (sMap.get(s[i]) !== tMap.get(t[i])) {
+    if (sLastSeenMap.get(s[i]) !== tLastSeenMap.get(t[i])) {
       return false;
     }
 
-    sMap.set(s[i], i);
-    tMap.set(t[i], i);
+    sLastSeenMap.set(s[i], i);
+    tLastSeenMap.set(t[i], i);
   }
 
   return true;
